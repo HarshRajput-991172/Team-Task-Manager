81f1b6648f76cad62833bdb15db1924c0980156a
HarshRajput-99117 <hsp0307042gmail.com>
2026-05-01 16:32:33 +0530

    COMMIT

--- app.py
+++ app.py
@@ -347,6 +347,7 @@ def change_password():
 
 
 # ================= RUN =================
+import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
